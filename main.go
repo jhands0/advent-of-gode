237af1ac3af794c680e8648dad1adbf2790f670f
jhands0 <jamie_hands@yahoo.co.uk>
2024-12-01 15:26:45 +0000

    Solved day 1 part 1

--- main.go
+++ main.go
@@ -3,9 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/echojc/aocutil"
 )
+
+func part1(lines []string) {
+	var left []int
+	var right []int
+
+	for _, line := range lines {
+		parts := strings.Fields(line)
+
+		leftNum, _ := strconv.Atoi(parts[0])
+		rightNum, _ := strconv.Atoi(parts[1])
+
+		left = append(left, leftNum)
+		right = append(right, rightNum)
+
+	}
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	totalDistance := 0
+
+	for i := 0; i < len(left); i++ {
+		distance := left[i] - right[i]
+
+		if distance < 0 {
+			distance *= -1
+		}
+
+		totalDistance += distance
+	}
+
+	fmt.Println(totalDistance)
+}
 
 func main() {
 	input, err := aocutil.NewInputFromFile("session_id")
@@ -13,10 +49,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	lines, err := input.Strings(2024, 2)
+	lines, err := input.Strings(2024, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(lines)
+	part1(lines)
 }
