1abe866be212092403f842c98ce38104fca9b1a5
Jamie Hands <jamie_hands0@protonmail.com>
2025-12-02 15:40:40 +0000

    Added day 1 part 2

--- 2025/01/main.go
+++ 2025/01/main.go
@@ -37,7 +37,35 @@ func part1(lines []string) {
 }
 
 func part2(lines []string) {
+	var direction byte
 
+	value := 50
+	clicks := 0
+	count := 0
+
+	for _, line := range lines {
+
+		direction = line[0]
+		turn, _ := strconv.Atoi(line[1:])
+		step := 1
+
+		if direction == 'L' {
+			step = 99
+		}
+
+		for range turn {
+			value = (value + step) % 100
+			if value == 0 {
+				clicks += 1
+			}
+		}
+		if value == 0 {
+			count += 1
+		}
+
+	}
+
+	fmt.Println(count, clicks)
 }
 
 func main() {
