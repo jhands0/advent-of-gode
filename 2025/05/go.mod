4f7883b0e6bfc25fefb884e411cb33d09e8770b5
Jamie Hands <jamie_hands0@protonmail.com>
2025-12-06 17:05:43 +0000

    Added template for day 5

---
+++ 2025/05/go.mod
@@ -0,0 +1,5 @@
+module 2025-05
+
+go 1.24.2
+
+require github.com/echojc/aocutil v0.0.0-20211125040159-e71bcb34f68f
---
+++ 2025/05/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	aocutil "github.com/echojc/aocutil"
+)
+
+func part1(lines []string) {
+
+}
+
+func part2(lines []string) {
+
+}
+
+func main() {
+	input, err := aocutil.NewInputFromFile("session_id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines, err := input.Strings(2025, 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	part1(lines)
+	part2(lines)
+}
