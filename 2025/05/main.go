80a0aba7f36a6d7c5803c3fd33972338fafed2e6
Jamie Hands <jamie_hands0@protonmail.com>
2025-12-15 17:38:40 +0000

    Added day 5 part 1

--- 2025/05/main.go
+++ 2025/05/main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	aocutil "github.com/echojc/aocutil"
 )
 
 func part1(lines []string) {
+	total := 0
+	freshIngredients := make(map[int]int)
+	var ingredients []string
 
+	for i, line := range lines {
+		if line == "" {
+			ingredients = lines[i:]
+			break
+		}
+
+		split := strings.Split(line, "-")
+		low, _ := strconv.Atoi(split[0])
+		up, _ := strconv.Atoi(split[1])
+
+		if value, found := freshIngredients[low]; found {
+			if value > up {
+				continue
+			}
+		}
+		freshIngredients[low] = up
+	}
+
+	for _, ingredientStr := range ingredients {
+		ingredient, _ := strconv.Atoi(ingredientStr)
+		for low, high := range freshIngredients {
+			if ingredient >= low && ingredient <= high {
+				total += 1
+				break
+			}
+		}
+	}
+
+	fmt.Println(total)
 }
 
 func part2(lines []string) {
